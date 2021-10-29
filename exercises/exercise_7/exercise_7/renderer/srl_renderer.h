b76d72c971c763cf3f4c9f6486ce82296d1d5190
henrique <hgdebarba@gmail.com>
2021-10-29 09:46:54 +0200

    assignment 3

--- exercises/exercise_7/exercise_7/renderer/srl_renderer.h
+++ exercises/exercise_7/exercise_7/renderer/srl_renderer.h
@@ -11,10 +11,7 @@
 #include "srl_types.h"
 
 
-
 namespace srl {
-
-
     class Renderer {
 
     public:
@@ -26,56 +23,35 @@ namespace srl {
                             CustomFrameBuffer <uint32_t> &fb,
                             CustomFrameBuffer <float> &db) {
 
-            // will copy all vertices since vts is const
-            std::vector<vertex> m_vts = vts;
+            // TODO exercise 7 / assignment 3
+            //  to make the Software Render Library work, you have to call all methods
+            //  in this class, in the right order and with the right parameters.
 
+            std::vector<vertex> _vts = vts; // copy all vertices from vts to _vts (since vts is a const)
+            std::vector<fragment> _frs;    // vector that will store the fragments
+            glm::mat4 modelViewProjection = vp * m; // the matrix that transform points from local space to clipping space
 
-            // 1. vertex shader equivalent
-            processVertices(vp * m, m_vts);
+            //  MIND THAT THE METHODS BELOW ARE NOT DECLARED/DEFINED IN THE RIGHT ORDER!
 
-            // 2. the fixed part of the pipeline
-            std::vector<fragment> m_frs;
-
-            // 2.1. create the primitives
-            assemblePrimitives(m_vts);
-
-            // 2.2. keep primitives in the visible volume
-            clipPrimitives();
-
-
-            // 2.3. move vertices to normalized device coordinates
-            divideByW();
-
-            // 2.4. normalized device coordinates to screen space
-            toScreenSpace(fb.W, fb.H);
-
-            // 2.5. reject primitives that are not facing towards the camera
-            //if(m_cullBackFaces)
-            backfaceCulling();
-
-            // 2.6. rasterization (generate fragments)
-            rasterPrimitives(m_frs);
-
-
-            processFragments(m_frs);
-
-            // 4. fragment operations and copy color to the frame buffer
-            writeToFrameBuffer(m_frs, fb, db);
         }
 
         virtual ~Renderer(){};
     private:
 
-        virtual void assemblePrimitives(const std::vector<vertex> &vts){};
+        virtual void assemblePrimitives(const std::vector<vertex> &vts) = 0;
+        // performs the perspective division
 
+        // remove all geometry outside the visible volume (performed in clipping space)
         virtual void clipPrimitives() = 0;
-
-        virtual void divideByW() = 0; // aka perspective divide
-
-        virtual void toScreenSpace(int width, int height) = 0;
-
+        // test if the surface of the primitive is visible to the camera
+        // only used when rendering triangles.
         virtual void backfaceCulling(){};
 
+        // (i.e. transforms from the clipping space to the normalized device coordinates)
+        virtual void divideByW() = 0;
+        // transform from normalized device coordinates to window coordinates
+        virtual void toScreenSpace(int width, int height) = 0;
+        // generate the fragments, with final window pixel locations, used to render the primitives
         virtual void rasterPrimitives(std::vector<fragment> &outFrs) = 0;
 
         // perform vertex operations in the vertex stream (i.e. the equivalent to a vertex shader)
@@ -96,6 +72,7 @@ namespace srl {
         }
 
         // fragment operations and copy color to frame buffer
+        // blending test and z/depth-buffer can come here
         static void writeToFrameBuffer(const std::vector<fragment> &frs, CustomFrameBuffer <uint32_t> &fb, CustomFrameBuffer <float> &db) {
 			int width = fb.W;
 			int height = fb.H;
@@ -105,20 +82,16 @@ namespace srl {
                 // make sure it is within framebuffer range (it won't be if we do not clip)
 				if (pos.x < 0 || pos.x >= width || pos.y < 0 || pos.y >= height)
 					continue;
-				
-				// blending test and z/depth-buffer will come here
-				// set the color of the pixel in the frame buffer
+
+				// z/depth-test algorithm:
 				if (frs[i].depth < db.valueAt(pos.x, pos.y)) {
+                    // is the new fragment closer? Then update the color and the depth buffer
 					fb.paintAt(pos.x, pos.y, Colors::toRGBA32(frs[i].col));
                     db.paintAt(pos.x, pos.y, frs[i].depth);
 				}
             }
         }
-
     };
-
-
-
 }
 
 #endif //GRAPHICSPROGRAMMINGEXERCISES_RENDERER_H
